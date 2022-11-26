16a9f039205d993029fa422a848b3dd92b6420f3
Connor Smiley <60119998+ConnorSmiley@users.noreply.github.com>
2022-11-26 16:19:13 +0900

    working on Cloud card component

--- src/Content/JSONContent.js
+++ src/Content/JSONContent.js
@@ -3,49 +3,57 @@ const JsonData = [
     "id":"1",
     "title": "placeholder",
     "date": "randomdate",
-    "content": "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+    "content": "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+    "img": ""
   },
   {
     "id":"2",
     "title": "placeholder2",
     "date": "randomdate",
-    "content": "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+    "content": "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+    "img": ""
   },
   {
     "id":"3",
     "title": "placeholder3",
     "date": "randomdate",
-    "content": "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+    "content": "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+    "img": ""
   },
   {
     "id":"4",
     "title": "placeholder4",
     "date": "randomdate",
-    "content": "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+    "content": "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+    "img": ""
   },
   {
     "id":"1",
     "title": "placeholder",
     "date": "randomdate",
-    "content": "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+    "content": "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+    "img": ""
   },
   {
     "id":"2",
     "title": "placeholder2",
     "date": "randomdate",
-    "content": "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+    "content": "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+    "img": ""
   },
   {
     "id":"3",
     "title": "placeholder3",
     "date": "randomdate",
-    "content": "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+    "content": "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+    "img": ""
   },
   {
     "id":"4",
     "title": "placeholder4",
     "date": "randomdate",
-    "content": "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+    "content": "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+    "img": ""
   },
 ]
 
