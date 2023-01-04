c3b3b6ce12a18cad2d81ed62e56145eea582ba9e
Connor Smiley <60119998+ConnorSmiley@users.noreply.github.com>
2023-01-04 20:35:09 +0900

    boiler

--- src/components/GlobalStyles.tsx
+++ src/components/GlobalStyles.tsx
@@ -1,12 +1,8 @@
 import React from 'react';
 import { createGlobalStyle } from 'styled-components';
-import tw, { theme, GlobalStyles as BaseStyles } from 'twin.macro';
+import {  GlobalStyles as BaseStyles } from 'twin.macro';
 
 const CustomStyles = createGlobalStyle`
-  body {
-    -webkit-tap-highlight-color: ${theme`colors.purple.500`};
-    ${tw`antialiased`}
-  }
 `;
 
 const GlobalStyles = () => (
